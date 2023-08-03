d7b3bd5ee553b56a92f51bb0a64307193a1d02fe
Studtrucker <studtrucker@posteo.de>
2023-08-03 07:22:54 +0200

    Entwicklung

---
+++ DatenImport/DatenImport.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Windows.Forms
+Imports Excel = Microsoft.Office.Interop.Excel
+Imports System.Collections.ObjectModel
+Imports Microsoft.Win32
+Imports Skireisen.Entities
+
+Public Module DatenImport
+    Private ReadOnly _ofdDokument As New Forms.OpenFileDialog
+    Private _Dokument As Excel.Workbook
+    Private _xlSheet As Excel.Worksheet
+    Private ReadOnly _xlCell As Excel.Range
+
+    Sub ImportMitarbeiterListe()
+        Dim xlApp As New Excel.Application
+
+        _Dokument = Nothing
+
+        _ofdDokument.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+        _ofdDokument.Filter = "Excel Dateien (*.xlsx)| *.xlsx"
+        _ofdDokument.FilterIndex = 1
+        _ofdDokument.RestoreDirectory = True
+
+        If _ofdDokument.ShowDialog = DialogResult.OK Then
+            _Dokument = xlApp.Workbooks.Open(_ofdDokument.FileName,, True)
+            If CheckExcelFileFormat(_Dokument) Then
+                'todo: Upload auf den SQL hier nicht mehr notwendig
+                'UploadMitarbeiterListe(ReadMitarbeiterExcelliste(_Dokument.ActiveSheet))
+            End If
+            _Dokument.Close()
+        End If
+
+    End Sub
+
+    Private Function ReadMitarbeiterExcelliste(Excelsheet As Excel.Worksheet) As Collection(Of Teilnehmer)
+        Dim CurrentRow = 2
+        Dim Teilnehmerliste As New Collection(Of Teilnehmer)
+        Dim RowCount = Excelsheet.UsedRange.Rows.Count
+        Do Until CurrentRow > RowCount
+            Dim Teilnehmer As New Teilnehmer With {
+            .Vorname = Excelsheet.UsedRange(CurrentRow, 1).Value,
+            .Name = Excelsheet.UsedRange(CurrentRow, 2).Value}
+            Teilnehmerliste.Add(Teilnehmer)
+            CurrentRow += 1
+        Loop
+        Return Teilnehmerliste
+    End Function
+
+
+    Private Function CheckExcelFileFormat(Excelfile As Excel.Workbook) As Boolean
+
+        Dim XlValid As Boolean
+
+        ' Excel Sheet (Überschrift Zeile 1, Daten Zeile 2 bis zum Dateiende)
+        _xlSheet = Excelfile.ActiveSheet
+
+        If _xlSheet IsNot Nothing Then
+            XlValid = _xlSheet.UsedRange.Columns.Count = 2
+
+            ' Check column caption
+            XlValid = XlValid And _xlSheet.Range("A1").Value = "Vorname"
+            XlValid = XlValid And _xlSheet.Range("B1").Value = "Nachname"
+
+            ' Check first data row
+            XlValid = XlValid And Not String.IsNullOrEmpty(_xlSheet.Range("A2").Value)
+            XlValid = XlValid And Not String.IsNullOrEmpty(_xlSheet.Range("B2").Value)
+
+        End If
+
+        Return XlValid
+
+    End Function
+
+End Module
--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,12 +1,20 @@
-﻿Imports Skireisen.Entities
+﻿Imports Microsoft.Win32
+Imports Skireisen.Entities
+Imports System.Reflection
 Imports System.ComponentModel
+Imports System.IO
+Imports System.IO.Compression
+Imports System.Windows.Shell
+Imports System.Xml.Serialization
 
 Class MainWindow
 
 #Region "Fields"
+
     Private _teilnehmerList As TeilnehmerCollection
     Private _teilnehmerListCollectionView As ICollectionView '... DataContext für das MainWindow
-
+    Private _skireiseListFile As FileInfo
+    Private _mRUSortedList As SortedList(Of Integer, String)
 
 #End Region
 
@@ -15,22 +23,22 @@ Class MainWindow
 
         ' 0. Zur InputBindings ein MouseBinding hinzufügen. Nur als Beispiel,
         '    um mit Strg und Doppelklick eine neue Liste anlegen zu können
-        Dim mg = New MouseGesture(MouseAction.LeftDoubleClick, ModifierKeys.Control)
-        Dim m = New MouseBinding(ApplicationCommands.[New], mg)
-        InputBindings.Add(m)
+        'Dim mg = New MouseGesture(MouseAction.LeftDoubleClick, ModifierKeys.Control)
+        'Dim m = New MouseBinding(ApplicationCommands.[New], mg)
+        'InputBindings.Add(m)
 
 
         ' 1. CommandBindings zur CommandBindings-Property des Window
         '    hinzufügen, um die Commands mit den entsprechenden Eventhandler zu verbinden
         CommandBindings.Add(New CommandBinding(ApplicationCommands.[New], AddressOf HandleListNewExecuted))
-        'CommandBindings.Add(New CommandBinding(ApplicationCommands.Open, AddressOf HandleListOpenExecuted))
+        CommandBindings.Add(New CommandBinding(ApplicationCommands.Open, AddressOf HandleListOpenExecuted))
         'CommandBindings.Add(New CommandBinding(ApplicationCommands.Save, AddressOf HandleListSaveExecuted, AddressOf HandleListSaveCanExecute))
         'CommandBindings.Add(New CommandBinding(ApplicationCommands.SaveAs, AddressOf HandleListSaveAsExecuted, AddressOf HandleListSaveCanExecute))
         'CommandBindings.Add(New CommandBinding(ApplicationCommands.Close, AddressOf HandleCloseExecuted))
         'CommandBindings.Add(New CommandBinding(ApplicationCommands.Help, AddressOf HandleHelpExecuted))
         'CommandBindings.Add(New CommandBinding(ApplicationCommands.Print, AddressOf HandleListPrintExecuted, AddressOf HandleListPrintCanExecute))
 
-        'CommandBindings.Add(New CommandBinding(NewFriend, AddressOf HandleFriendNewExecuted, AddressOf HandleFriendNewCanExecute))
+        CommandBindings.Add(New CommandBinding(ImportTeilnehmerliste, AddressOf HandleImportTeilnehmerExecuted, AddressOf HandleImportTeilnehmerCanExecute))
 
     End Sub
 
@@ -38,9 +46,6 @@ Class MainWindow
 
 #Region "EventHandler der CommandBindings"
     Private Sub HandleListNewExecuted(sender As Object, e As ExecutedRoutedEventArgs)
-
-        MessageBox.Show("Hallo")
-        Exit Sub
 
         If _teilnehmerList IsNot Nothing Then
             Dim rs As MessageBoxResult = MessageBox.Show("Möchten Sie die aktuelle Liste noch speichern?", "", MessageBoxButton.YesNoCancel)
@@ -60,6 +65,146 @@ Class MainWindow
             'NewFriend.Execute(Nothing, Me)
         End If
 
+    End Sub
+
+    Private Sub HandleListOpenExecuted(sender As Object, e As ExecutedRoutedEventArgs)
+        Dim dlg As OpenFileDialog = New OpenFileDialog With {.Filter = "*.ski|*.ski"}
+        If dlg.ShowDialog = True Then
+            OpenSkireiseList(dlg.FileName)
+        End If
+    End Sub
+
+    Private Sub HandleMostRecentClick(sender As Object, e As RoutedEventArgs)
+        OpenSkireiseList(TryCast(sender, MenuItem).Header.ToString())
+    End Sub
+
+    Private Sub HandleImportTeilnehmerExecuted(sender As Object, e As ExecutedRoutedEventArgs)
+
+        DatenImport.ImportMitarbeiterListe()
+
+    End Sub
+
+    Private Sub HandleImportTeilnehmerCanExecute(sender As Object, e As CanExecuteRoutedEventArgs)
+        e.CanExecute = _teilnehmerList IsNot Nothing
+    End Sub
+
+
+#End Region
+
+#Region "Helper-Methoden"
+    Private Sub OpenSkireiseList(fileName As String)
+        If _skireiseListFile IsNot Nothing AndAlso fileName.Equals(_skireiseListFile.FullName) Then
+            MessageBox.Show("Die Liste " & fileName & " ist bereits geöffnet")
+            Exit Sub
+        End If
+
+        If Not File.Exists(fileName) Then
+            MessageBox.Show("Die Datei existiert nicht")
+            Exit Sub
+        End If
+
+        ' Datei enzippen und deserialisieren
+        Dim serializer As XmlSerializer = New XmlSerializer(GetType(TeilnehmerCollection))
+        Dim loadedFriendCollection As TeilnehmerCollection = Nothing
+        Using fs = New FileStream(fileName, FileMode.Open)
+            Using zipStream = New GZipStream(fs, CompressionMode.Decompress)
+                Try
+                    loadedFriendCollection = TryCast(serializer.Deserialize(zipStream), TeilnehmerCollection)
+                Catch ex As InvalidDataException
+                    MessageBox.Show("Datei ungültig: " & ex.Message)
+                    Exit Sub
+                End Try
+            End Using
+        End Using
+
+        _teilnehmerList = Nothing
+
+        _skireiseListFile = New FileInfo(fileName)
+        QueueMostRecentFilename(fileName)
+        SetView(loadedFriendCollection)
+        Title = "Skireisen - " & fileName
+
+    End Sub
+
+    Private Sub QueueMostRecentFilename(fileName As String)
+        Dim max As Integer = 0
+        For Each i In _mRUSortedList.Keys
+            If i > max Then max = i
+        Next
+
+        Dim keysToRemove As List(Of Integer) = New List(Of Integer)
+        For Each kvp As KeyValuePair(Of Integer, String) In _mRUSortedList
+            If kvp.Value.Equals(fileName) Then keysToRemove.Add(kvp.Key)
+        Next
+
+        For Each i As Integer In keysToRemove
+            _mRUSortedList.Remove(i)
+        Next
+
+        _mRUSortedList.Add(max + 1, fileName)
+
+        If _mRUSortedList.Count > 5 Then
+            Dim min As Integer = Integer.MaxValue
+            For Each i As Integer In _mRUSortedList.Keys
+                If i < min Then min = i
+            Next
+            _mRUSortedList.Remove(min)
+        End If
+
+        RefreshMostRecentMenu()
+
+    End Sub
+
+    Private Sub RefreshMostRecentMenu()
+        mostrecentlyUsedMenuItem.Items.Clear()
+        RefreshMenuInApplication()
+        RefreshJumpListInWinTaskbar()
+    End Sub
+
+    Private Sub RefreshMenuInApplication()
+        For i = _mRUSortedList.Values.Count - 1 To 0 Step -1
+            Dim mi As MenuItem = New MenuItem()
+            mi.Header = _mRUSortedList.Values(i)
+            AddHandler mi.Click, AddressOf HandleMostRecentClick
+            mostrecentlyUsedMenuItem.Items.Add(mi)
+        Next
+
+        If mostrecentlyUsedMenuItem.Items.Count = 0 Then
+            Dim mi As MenuItem = New MenuItem()
+            mi.Header = "keine"
+            mostrecentlyUsedMenuItem.Items.Add(mi)
+        End If
+    End Sub
+    Private Sub RefreshJumpListInWinTaskbar()
+
+        Dim jumplist = New JumpList With {
+            .ShowFrequentCategory = False,
+            .ShowRecentCategory = False}
+
+        Dim jumptask = New JumpTask With {
+            .CustomCategory = "Release Notes",
+            .Title = "FriendStorageReleaseNotes",
+            .Description = "Zeigt die ReleaseNotes zu FriendStorage an",
+            .ApplicationPath = "C:\Windows\notepad.exe",
+            .IconResourcePath = "C:\Windows\notepad.exe",
+            .WorkingDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location),
+            .Arguments = "ReleaseNotes.txt"}
+
+        jumplist.JumpItems.Add(jumptask)
+
+        ' Hinweis Die JumpPath - Elemente sind nur sichtbar, wenn die ".friends"-Dateiendung
+        ' unter Windows mit FriendStorage assoziiert wird (kann durch Installation via Setup-Projekt erreicht werden,
+        ' das auch in den Beispielen enthalten ist, welches die dafür benötigten Werte in die Registry schreibt)
+
+        For i = _mRUSortedList.Values.Count - 1 To 0 Step -1
+            Dim jumpPath = New JumpPath With {
+                .CustomCategory = "Zuletzt geöffnet",
+                .Path = _mRUSortedList.Values(i)}
+
+            jumplist.JumpItems.Add(jumpPath)
+        Next
+
+        JumpList.SetJumpList(Application.Current, jumplist)
     End Sub
 
 #End Region

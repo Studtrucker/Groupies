52d71b2d46e279cbe42ac2696e91b2d40885c27d
Andreas Studtrucker <andreas.studtrucker@konecranes.com>
2023-08-08 08:57:53 +0200

    Entwicklung

--- Commands/SkireisenBefehle.vb
+++ Commands/SkireisenBefehle.vb
@@ -1,13 +1,21 @@
 ﻿Public Module SkireisenBefehle
     Private ReadOnly _importiereTeilnehmer As RoutedUICommand
+    Private ReadOnly _teilnehmerKoennenstufeVergabe As RoutedUICommand
 
     Sub New()
         _importiereTeilnehmer = New RoutedUICommand("Teilnehmerliste importieren", "ImportTeilnehmerliste", GetType(SkireisenBefehle))
+        _teilnehmerKoennenstufeVergabe = New RoutedUICommand("Levelbeurteilung Teilnehmer", "TeilnehmerKoennenstufeVergabe", GetType(SkireisenBefehle))
     End Sub
 
     Public ReadOnly Property ImportTeilnehmerliste As RoutedUICommand
         Get
             Return _importiereTeilnehmer
+        End Get
+    End Property
+
+    Public ReadOnly Property TeilnehmerKoennenstufeVergabe As RoutedUICommand
+        Get
+            Return _teilnehmerKoennenstufeVergabe
         End Get
     End Property
 

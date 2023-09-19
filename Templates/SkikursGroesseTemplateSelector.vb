9c58d50932f0662598870d7a2177c7a169aec490
Andreas Studtrucker <andreas.studtrucker@konecranes.com>
2023-09-19 09:39:47 +0200

    Entwicklung

---
+++ Templates/SkikursGroesseTemplateSelector.vb
@@ -0,0 +1,29 @@
+﻿Public Class SkikursGroesseTemplateSelector
+    Inherits DataTemplateSelector
+    Public KleineGruppe As DataTemplate
+    Public NormaleGruppe As DataTemplate
+    Public GrosseGruppe As DataTemplate
+
+    Public Overrides Function SelectTemplate(item As Object, container As DependencyObject) As DataTemplate
+        If GrosseGruppe Is Nothing Then
+            GrosseGruppe = TryCast(Application.Current.FindResource("GrosseGruppe"), DataTemplate)
+        End If
+        If NormaleGruppe Is Nothing Then
+            NormaleGruppe = TryCast(Application.Current.FindResource("NormaleGruppe"), DataTemplate)
+        End If
+        If KleineGruppe Is Nothing Then
+            KleineGruppe = TryCast(Application.Current.FindResource("KleineGruppe"), DataTemplate)
+        End If
+        Dim SK = TryCast(item, Skischule.Entities.Skikurs)
+        If SK IsNot Nothing Then
+            If SK.AnzahlMitglieder < 7 Then
+                Return KleineGruppe
+            End If
+            If SK.AnzahlMitglieder >= 7 And SK.AnzahlMitglieder < 14 Then
+                Return NormaleGruppe
+            End If
+            Return GrosseGruppe
+        End If
+        Return MyBase.SelectTemplate(item, container)
+    End Function
+End Class

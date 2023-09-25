66a4a4f80fa4b193cd001e8ca257f2b966205100
Andreas Studtrucker <andreas.studtrucker@konecranes.com>
2023-09-25 08:25:27 +0200

    Entwicklung

--- Converters/AnzahlConverter.vb
+++ Converters/AnzahlConverter.vb
@@ -2,19 +2,19 @@
 Namespace Converters
 
 
-    Public Class AnzahlKleiner5ToBooleanConverter
+    Public Class Anzahl2TextConverter
         Implements IValueConverter
 
-        Public Anzahl As Integer
+        Public Text As String
 
         Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
-            If Not TypeOf value Is Integer Then Return DependencyProperty.UnsetValue
-            Return If(value < 5, True, False)
+            If TypeOf value IsNot Integer Then Return DependencyProperty.UnsetValue
+            Return If(value < 5, Text = "Kleiner5")
         End Function
 
         Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
-            If Not TypeOf value Is Integer Then Return DependencyProperty.UnsetValue
-            Return If(value < 5, True, False)
+            If TypeOf value IsNot String Then Return DependencyProperty.UnsetValue
+            Return If(value = "Kleiner5", True, False)
         End Function
     End Class
 

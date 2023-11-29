b38204018f32b5f27009dc09976560f4be86be52
Studtrucker <studtrucker@posteo.de>
2023-11-29 21:59:10 +0100

    Development

--- Window1.xaml.vb
+++ Window1.xaml.vb
@@ -1,4 +1,8 @@
-﻿Public Class Window1
+﻿Imports System.IO
+Imports System.IO.IsolatedStorage
+Imports System.Xml.Serialization
+
+Public Class Window1
     Sub New()
 
         ' Dieser Aufruf ist für den Designer erforderlich.
@@ -8,8 +12,67 @@
         Groupies.Services.CurrentDataService.Skiclub = New Entities.Skiclub
         Groupies.Services.CurrentDataService.Skiclub.Levellist = Groupies.Services.CreateLevels()
 
-        LevelView.DataContext = Groupies.Services.CurrentDataService.Skiclub.Levellist(4)
-
+        LoadLastSkischule()
     End Sub
 
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        InstructorView.DataContext = Groupies.Services.CurrentDataService.Skiclub.Instructorlist(0)
+    End Sub
+
+    Private Sub LoadLastSkischule()
+        ' Die letze Skischule aus dem IsolatedStorage holen.
+        Try
+            Dim x = ""
+            Using iso = IsolatedStorageFile.GetUserStoreForAssembly()
+                Using stream = New IsolatedStorageFileStream("LastSkischule", FileMode.Open, iso)
+                    Using reader = New StreamReader(stream)
+                        x = reader.ReadLine
+                    End Using
+                End Using
+
+            End Using
+            If File.Exists(x) Then Me.OpenSkischule(x)
+        Catch ex As FileNotFoundException
+        End Try
+    End Sub
+    Private Sub OpenSkischule(fileName As String)
+        Dim _skischuleListFile = New FileInfo(fileName)
+
+        'If _skischuleListFile IsNot Nothing AndAlso fileName.Equals(_skischuleListFile.FullName) Then
+        '    MessageBox.Show("Groupies " & fileName & " ist bereits geöffnet")
+        '    Exit Sub
+        'End If
+
+        'If Not File.Exists(fileName) Then
+        '    MessageBox.Show("Die Datei existiert nicht")
+        '    Exit Sub
+        'End If
+
+        Dim loadedSkischule = OpenXML(fileName)
+        'Dim loadedSkischule = OpenZIP(fileName)
+        If loadedSkischule Is Nothing Then Exit Sub
+
+        Groupies.Services.CurrentDataService.Skiclub = loadedSkischule
+
+        '_skischuleListFile = New FileInfo(fileName)
+        'QueueMostRecentFilename(fileName)
+        'SetView(loadedSkischule)
+        'Title = "Groupies - " & fileName
+
+    End Sub
+    Private Function OpenXML(fileName As String) As Entities.Skiclub
+        Dim serializer = New XmlSerializer(GetType(Entities.Skiclub))
+        Dim loadedSkiclub As Entities.Skiclub = Nothing
+
+        ' Datei deserialisieren
+        Using fs = New FileStream(fileName, FileMode.Open)
+            Try
+                loadedSkiclub = TryCast(serializer.Deserialize(fs), Entities.Skiclub)
+            Catch ex As InvalidDataException
+                MessageBox.Show("Datei ungültig: " & ex.Message)
+                Return Nothing
+            End Try
+        End Using
+        Return loadedSkiclub
+    End Function
 End Class

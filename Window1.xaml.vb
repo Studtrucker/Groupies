fa7f780a0d3bd00f8f0379be3ad1393ac8925556
Studtrucker <studtrucker@posteo.de>
2024-01-11 23:15:34 +0100

    Development

--- Window1.xaml.vb
+++ Window1.xaml.vb
@@ -3,56 +3,67 @@ Imports System.IO
 Imports System.IO.IsolatedStorage
 Imports System.Xml.Serialization
 Imports Groupies.Commands
+Imports Groupies.Entities
 Imports CDS = Groupies.Services.CurrentDataService
 
 Public Class Window1
 
-    'Private _levelListCollectionView As ICollectionView
+#Region "Fields"
+    Private _teilnehmerListCollectionView As ICollectionView
+    Private _gruppenListCollectionView As ICollectionView
+    Private _uebungsleiterListCollectionView As ICollectionView
+#End Region
 
-    'Sub New()
+#Region "Constructor"
 
-    '    ' Dieser Aufruf ist für den Designer erforderlich.
-    '    InitializeComponent()
+    Sub New()
 
-    '    ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
+        ' Dieser Aufruf ist für den Designer erforderlich.
+        InitializeComponent()
 
-    '    'Services.Service.LoadLastSkischule()
-    '    'If Groupies.Services.CurrentDataService.Skiclub Is Nothing Then
-    '    'Services.Service.OpenSkischule("2023StubaiTest.ski")
-    '    'End If
-    '    _levelListCollectionView = New ListCollectionView(Groupies.Services.CurrentDataService.Skiclub.Levellist)
-    '    _levelListCollectionView.SortDescriptions.Add(New SortDescription("SortNumber", ListSortDirection.Ascending))
-    '    DataContext = _levelListCollectionView
+        ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
+        _teilnehmerListCollectionView = New ListCollectionView(New ParticipantCollection)
+        _gruppenListCollectionView = New ListCollectionView(New GroupCollection)
+        _uebungsleiterListCollectionView = New ListCollectionView(New InstructorCollection)
 
-    'End Sub
+    End Sub
 
-    'Sub New()
+#End Region
 
-    '    ' Dieser Aufruf ist für den Designer erforderlich.
-    '    InitializeComponent()
+#Region "Window-Events"
+    Private Sub Window1_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
 
-    '    ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
-    '    _skillListCollectionView = New ListCollectionView(New SkillCollection())
+        ' 1. CommandBindings zur CommandBindings-Property des Window
+        '    hinzufügen, um die Commands mit den entsprechenden Eventhandler zu verbinden
 
-    'End Sub
+        setView(Groupies.Services.CurrentDataService.Skiclub)
 
-    '' Zur Ausführung des Handles HandleNewSkillExecuted erstellen, kann auf dem Mutter Window ein RoutedEvent registriert werden.
-    '' Siehe auch EventTrigger (= Ereignisauslöser) Kapitel 11 »Styles, Trigger und Templates«
+    End Sub
 
-    'Private Sub Window1_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
-    '    CommandBindings.Add(New CommandBinding(SkiclubCommands.NeuerSkill, AddressOf HandleNewSkillExecuted))
-    'End Sub
+#End Region
 
-    'Private Sub HandleNewSkillCanExecute(sender As Object, e As CanExecuteRoutedEventArgs)
-    '    e.CanExecute = True
-    'End Sub
+#Region "Helper-Methoden"
+    Private Sub setView(Skikursliste As Skiclub)
+        ' Neue ListCollectionView laden
+        _teilnehmerListCollectionView = New ListCollectionView(CDS.Skiclub.Participantlist)
+        ' ListCollectionView nach Teilnehmer in Gruppen filtern
+        If _teilnehmerListCollectionView.CanFilter Then
+            _teilnehmerListCollectionView.Filter = Function(x As Participant) x.IsNotInGroup = True
+        End If
+    End Sub
+#End Region
 
-    'Private Sub HandleNewSkillExecuted(sender As Object, e As ExecutedRoutedEventArgs)
-    '    Dim dlg = New NewSkillDialog With {.Owner = Me, .WindowStartupLocation = WindowStartupLocation.CenterOwner}
-    '    If dlg.ShowDialog = True Then
-    '        CDS.Skiclub.Levellist(_levelListCollectionView.CurrentItem).LevelSkills.Add(dlg.Skill)
-    '        'skillListCollectionView.MoveCurrentTo(dlg.Skill)
-    '        'SkillsDataGrid.ScrollIntoView(dlg.Skill)
-    '    End If
-    'End Sub
+    Private Sub MenuItem_Click_1(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub ParticipantsToDistributeDataGrid_SendByMouseDown(sender As Object, e As MouseButtonEventArgs)
+
+    End Sub
+
+    Private Sub ParticipantsToDistributeDataGrid_ReceiveByDrop(sender As Object, e As DragEventArgs)
+
+    End Sub
+
+
 End Class

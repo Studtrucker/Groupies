94babb1668af052bf4273817ea4e211e30093962
Andreas Studtrucker <andreas.studtrucker@konecranes.com>
2024-09-24 15:17:00 +0200

    TestProjekt ergänzt

---
+++ AppController.vb
@@ -0,0 +1,33 @@
+﻿Imports System.IO
+Imports Groupies.Entities
+
+Public Class AppController
+    ''' <summary>
+    ''' Der aktuell verwaltete Club
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property CurrentGroupies As Skiclub
+
+
+    ''' <summary>
+    ''' Lädt Daten aus einer XML Datei
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function LoadFromXML(Filename As String) As String
+        If Filename.Contains("/") OrElse Filename.Contains("\") OrElse Filename.Contains(" ") Then
+            Return "Bitte geben Sie einen Dateinamen ohne Schrägstriche oder Leerzeichen ein."
+        ElseIf Not File.Exists(Format("{0}.xml", Filename)) Then
+            Return "Die Datei existiert nicht"
+        End If
+        Return "Es wurde keine Datei geladen"
+    End Function
+
+    ''' <summary>
+    ''' Lädt Daten aus einer JSON Datei
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function LoadFromJson(Filename As String) As String
+        Return "Es wurde keine Datei geladen"
+    End Function
+
+End Class

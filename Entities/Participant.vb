f7561d501a4b6fe66c26023d2eefd9c96614f3b3
Andreas Studtrucker <andreas.studtrucker@konecranes.com>
2024-09-24 11:38:44 +0200

    Kommentierung

--- Entities/Participant.vb
+++ Entities/Participant.vb
@@ -16,14 +16,30 @@ Namespace Entities
             _ParticipantID = Guid.NewGuid()
         End Sub
 
+
+        ''' <summary>
+        ''' Das ist eine eindeutige Kennzeichnung für den Teilnehmer 
+        ''' </summary>
+        ''' <returns></returns>
         Public Property ParticipantID As Guid
 
+        ''' <summary>
+        ''' Der Nachname des Teilnehmers
+        ''' </summary>
+        ''' <returns></returns>
         Public Property ParticipantLastName As String
 
-
+        ''' <summary>
+        ''' Der Vorname des Teilnehmers
+        ''' </summary>
+        ''' <returns></returns>
         <Required(AllowEmptyStrings:=False, ErrorMessage:="Der Vorname ist eine Pflichtangabe")>
         Public Property ParticipantFirstName As String
 
+        ''' <summary>
+        ''' Der Vor- und Nachname
+        ''' </summary>
+        ''' <returns></returns>
         Public ReadOnly Property ParticipantFullName As String
             Get
                 If _ParticipantFirstName Is Nothing Then
@@ -36,30 +52,60 @@ Namespace Entities
             End Get
         End Property
 
-
+        ''' <summary>
+        ''' Der Leistungsstand des Teilnehmers
+        ''' </summary>
+        ''' <returns></returns>
         Public Property ParticipantLevel As Level
 
+        ''' <summary>
+        ''' Gruppenmitglied in der Gruppe 
+        ''' mit dem Gruppenkennzeichen
+        ''' </summary>
+        ''' <returns></returns>
         Public Property MemberOfGroup As Guid
 
+
+        ''' <summary>
+        ''' Gruppenmitglied in der Gruppe 
+        ''' mit dem Gruppennamen 
+        ''' </summary>
+        ''' <returns></returns>
         Public ReadOnly Property MemberOfGroup_Naming() As String
             Get
                 Return CDS.Skiclub.Grouplist.Where(Function(x) x.GroupID.Equals(MemberOfGroup)).DefaultIfEmpty(New Group With {.GroupNaming = String.Empty}).Single.GroupNaming
             End Get
         End Property
 
+
+        ''' <summary>
+        ''' Wird Gruppenmitglied von der Gruppe
+        ''' mit dem Gruppenkennzeichen
+        ''' </summary>
+        ''' <param name="GroupID"></param>
         Public Sub SetAsGroupMember(GroupID As Guid)
             MemberOfGroup = GroupID
             _IsGroupMember = True
         End Sub
+
+        ''' <summary>
+        ''' Wird als Gruppenmitgleid gekennzeichnet
+        ''' </summary>
         Public Sub SetAsGroupMember()
             _IsGroupMember = True
         End Sub
 
+        ''' <summary>
+        ''' Wird als Gruppenmitglied entfernt
+        ''' </summary>
         Public Sub RemoveFromGroup()
             MemberOfGroup = Nothing
             _IsGroupMember = False
         End Sub
 
+        ''' <summary>
+        ''' Setzt und liest die Gruppenmitgliedschaft
+        ''' </summary>
         Private _IsGroupMember As Boolean
         Public Property IsGroupMember As Boolean
             Get
@@ -70,6 +116,11 @@ Namespace Entities
             End Set
         End Property
 
+
+        ''' <summary>
+        ''' Liest die umgekehrte Gruppenmitgliedschaft
+        ''' </summary>
+        ''' <returns></returns>
         Public ReadOnly Property IsNotInGroup As Boolean
             Get
                 Return Not _IsGroupMember

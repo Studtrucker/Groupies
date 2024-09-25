9209a77383dbedcf746e9a5a3c35dd6fc7dda9ea
Andreas Studtrucker privat <studtrucker@posteo.de>
2024-09-25 07:34:49 +0200

    Tests

--- AppController.vb
+++ AppController.vb
@@ -16,10 +16,10 @@ Public Class AppController
     Public Shared Function LoadFromXML(Filename As String) As String
         If Filename.Contains("/") OrElse Filename.Contains("\") OrElse Filename.Contains(" ") Then
             Return "Bitte geben Sie einen Dateinamen ohne Schrägstriche oder Leerzeichen ein."
-        ElseIf Not File.Exists(Format("{0}.xml", Filename)) Then
-            Return "Die Datei existiert nicht"
+        ElseIf Not File.Exists(String.Format("{0}.xml", Filename)) Then
+            Return String.Format("Die Datei {1} im Ordner {0} existiert nicht.", Environment.CurrentDirectory, String.Format("{0}.xml", Filename))
         End If
-        Return "Es wurde keine Datei geladen"
+        Return String.Format("Die Datei {0} wurde geladen.", String.Format("{0}.xml", Filename))
     End Function
 
     ''' <summary>
@@ -27,7 +27,12 @@ Public Class AppController
     ''' </summary>
     ''' <returns></returns>
     Public Shared Function LoadFromJson(Filename As String) As String
-        Return "Es wurde keine Datei geladen"
+        If Filename.Contains("/") OrElse Filename.Contains(" ") OrElse Filename.Contains("\") Then
+            Return "Bitte geben Sie einen Dateinamen ohne Schrägstriche oder Leerzeichen ein."
+        ElseIf Not File.Exists(String.Format("{0}.xml", Filename)) Then
+            Return String.Format("Die Datei {1} im Ordner {0} existiert nicht.", Environment.CurrentDirectory, String.Format("{0}.json", Filename))
+        End If
+        Return String.Format("Die Datei {0} wurde geladen.", String.Format("{0}.xml", Filename))
     End Function
 
 End Class
---
+++ GroupiesTestsVB/AppControllerTests.vb
@@ -0,0 +1,38 @@
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Groupies
+
+Namespace GroupiesTestsVB
+    <TestClass>
+    Public Class AppControllerTests
+        <TestMethod>
+        Sub TestLoadJson()
+            Dim filename As String
+            filename = "Test"
+            Assert.AreEqual(String.Format("Die Datei {0} wurde geladen.", String.Format("{0}.json", filename)), AppController.LoadFromJson(filename))
+        End Sub
+        <TestMethod>
+        Sub TestLoadXML()
+            Dim filename As String
+            filename = "test"
+            Assert.AreEqual(String.Format("Die Datei {0} wurde geladen.", String.Format("{0}.xml", filename)), AppController.LoadFromXML(filename))
+        End Sub
+
+        <TestMethod>
+        Public Sub TestInvalidFilenames()
+            Assert.AreEqual("Bitte geben Sie einen Dateinamen ohne Schrägstriche oder Leerzeichen ein.", AppController.LoadFromJson("invalid\\filename"))
+            Assert.AreEqual("Bitte geben Sie einen Dateinamen ohne Schrägstriche oder Leerzeichen ein.", AppController.LoadFromJson("invalid/filename"))
+            Assert.AreEqual("Bitte geben Sie einen Dateinamen ohne Schrägstriche oder Leerzeichen ein.", AppController.LoadFromJson("invalid filename"))
+        End Sub
+
+        <TestMethod>
+        Public Sub TestNotExistingFile()
+            Dim filename As String
+            filename = "tester"
+            Assert.AreEqual(String.Format("Die Datei {0} im Ordner {1} existiert nicht.", filename & ".json", Environment.CurrentDirectory), AppController.LoadFromJson(filename))
+            Assert.AreEqual(String.Format("Die Datei {0} im Ordner {1} existiert nicht.", filename & ".xml", Environment.CurrentDirectory), AppController.LoadFromXML(filename))
+        End Sub
+
+    End Class
+
+End Namespace
+

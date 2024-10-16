56a3fae5547afd057a8df129264d8c3a3a28262d
Andreas Studtrucker privat <studtrucker@posteo.de>
2024-10-16 06:46:56 +0200

    Test

--- MSTestGroupiesTests/ExcelImportTests.vb
+++ MSTestGroupiesTests/ExcelImportTests.vb
@@ -6,6 +6,13 @@ Imports Groupies.Services
 Public Class ExcelImportTests
     <TestMethod>
     Public Sub ImportTeilnehmer()
-        Assert.AreEqual(True, OpenExcelFile("C:\Users\studtan\OneDrive\Dokumente\Reisen\Stubaital\Reise_2024_Teilnehmer.xlsx"))
+        Dim Pfad
+        If Environment.MachineName = "DESKTOP-JGIR9SQ" Then
+            Pfad = "C:\Users\studt_era90oc\OneDrive\Dokumente\Reisen\Stubaital\Reise_2024_Teilnehmer.xlsx"
+        Else
+            Pfad = "C:\Users\studtan\OneDrive\Dokumente\Reisen\Stubaital\Reise_2024_Teilnehmer.xlsx"
+        End If
+
+        Assert.AreEqual(True, OpenExcelFile(Pfad))
     End Sub
 End Class

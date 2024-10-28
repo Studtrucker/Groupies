6cc1abec2e333e3d460cf18938d76c93171a78d9
Andreas Studtrucker <andreas.studtrucker@konecranes.com>
2024-10-28 08:04:17 +0100

    Korrektur

--- GroupiesTestsFramwork4/ImportServiceTests.vb
+++ GroupiesTestsFramwork4/ImportServiceTests.vb
@@ -3,7 +3,13 @@
 Public Class ImportServiceTests
     <TestMethod>
     Public Sub TestStarteOpenFileDialog()
-        Dim Pfad = StarteOpenFileDialog("C:\Users\studt_era90oc\OneDrive\Dokumente\Reisen\Stubaital\", "2024_TeilnehmerBearbeitet.xlsx")
-        Assert.AreEqual("C:\Users\studt_era90oc\OneDrive\Dokumente\Reisen\Stubaital\2024_TeilnehmerBearbeitet.xlsx", Pfad)
+        Dim Pfad
+        If Environment.MachineName = "DESKTOP-JGIR9SQ" Then
+            Pfad = "C:\Users\studt_era90oc\OneDrive\Dokumente\Reisen\Stubaital\2024_TeilnehmerBearbeitet.xlsx"
+        Else
+            Pfad = "C:\Users\studtan\OneDrive\Dokumente\Reisen\Stubaital\2024_TeilnehmerBearbeitet.xlsx"
+        End If
+        Dim Result = StarteOpenFileDialog("C:\Users\studt_era90oc\OneDrive\Dokumente\Reisen\Stubaital", "2024_TeilnehmerBearbeitet.xlsx")
+        Assert.AreEqual(Pfad, Result)
     End Sub
 End Class

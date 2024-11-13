b20f02b1329b09d2c012b8844a332115db7e7f43
Andreas Studtrucker privat <studtrucker@posteo.de>
2024-11-14 00:10:57 +0100

    Listen

--- Dialogs/TeilnehmerSuchErgebnis.xaml.vb
+++ Dialogs/TeilnehmerSuchErgebnis.xaml.vb
@@ -11,7 +11,15 @@ Public Class TeilnehmerSuchErgebnis
         ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
 
         'Dim x = Suchergebnis.ToList.Select(Function(Gr) Gr.Mitgliederliste.Select(Function(Tn) New With {.TeilnehmerID = Tn.TeilnehmerID, .Name = Tn.VorUndNachname, .GruppenID = Gr.GruppenID, .GruppenName = Gr.Benennung, .Trainer = Gr.Trainer.VorUndNachname, .Gruppenstufe = Gr.Leistungsstufe.Benennung}))
-        Dim x = Suchergebnis.ToList.Select(Function(Gr) Gr.Mitgliederliste.Select(Function(Tn) New With {.Name = Tn.VorUndNachname, .GruppenName = Gr.Benennung})).ToList
+        Dim y = Suchergebnis.ToList.Select(Function(Gr) Gr.Mitgliederliste.Select(Function(Tn) New With {.Name = Tn.VorUndNachname, .GruppenName = Gr.Benennung})).ToList
+        Dim a = New With {.Name = "Andreas", .Nachname = "Studtrucker"}
+        Dim r = New With {.Name = "Rainer", .Nachname = "Studtrucker"}
+
+        Dim n As New List(Of Object) From {y.Select(Function(c) c)}
+
+        Dim x As List(Of Object)
+        x = New List(Of Object)
+        x.AddRange({a, r})
 
         DataContext = x
 

fcf824c8563d4422bb196974803ab5a20da70094
Andreas Studtrucker privat <studtrucker@posteo.de>
2024-11-14 00:24:38 +0100

    Jretzt gehts

--- Dialogs/TeilnehmerSuchErgebnis.xaml.vb
+++ Dialogs/TeilnehmerSuchErgebnis.xaml.vb
@@ -11,17 +11,17 @@ Public Class TeilnehmerSuchErgebnis
         ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
 
         'Dim x = Suchergebnis.ToList.Select(Function(Gr) Gr.Mitgliederliste.Select(Function(Tn) New With {.TeilnehmerID = Tn.TeilnehmerID, .Name = Tn.VorUndNachname, .GruppenID = Gr.GruppenID, .GruppenName = Gr.Benennung, .Trainer = Gr.Trainer.VorUndNachname, .Gruppenstufe = Gr.Leistungsstufe.Benennung}))
-        Dim y = Suchergebnis.ToList.Select(Function(Gr) Gr.Mitgliederliste.Select(Function(Tn) New With {.Name = Tn.VorUndNachname, .GruppenName = Gr.Benennung})).ToList
+
+        Dim l As New List(Of Object)
+        Suchergebnis.ToList.ForEach(Sub(Gr) Gr.Mitgliederliste.ToList.ForEach(Sub(Tn) l.Add(New With {.Name = Tn.VorUndNachname, .GruppenName = Gr.Benennung})))
         Dim a = New With {.Name = "Andreas", .Nachname = "Studtrucker"}
         Dim r = New With {.Name = "Rainer", .Nachname = "Studtrucker"}
-
-        Dim n As New List(Of Object) From {y.Select(Function(c) c)}
 
         Dim x As List(Of Object)
         x = New List(Of Object)
         x.AddRange({a, r})
 
-        DataContext = x
+        DataContext = l
 
     End Sub
 

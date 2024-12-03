f58fc2f228fb1cffb5efe339fdb7f4f8d62ccc11
Andreas Studtrucker <andreas.studtrucker@konecranes.com>
2024-12-03 16:19:32 +0100

    Trainerübersicht

--- UserControls/Traineruebersicht.xaml.vb
+++ UserControls/Traineruebersicht.xaml.vb
@@ -1,3 +1,17 @@
-﻿Public Class Traineruebersicht
+﻿Imports System.ComponentModel
+Imports AppCon = Groupies.Controller.AppController
 
+
+Public Class Traineruebersicht
+    Private _TrainerCollectionView As ICollectionView
+
+    Private Sub Handle_Traineruebersicht_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+
+        _TrainerCollectionView = New ListCollectionView(AppCon.CurrentClub.AlleTrainer)
+        If _TrainerCollectionView.CanSort Then
+            _TrainerCollectionView.SortDescriptions.Add(New SortDescription("Nachname", ListSortDirection.Ascending))
+        End If
+        DataContext = _TrainerCollectionView
+
+    End Sub
 End Class

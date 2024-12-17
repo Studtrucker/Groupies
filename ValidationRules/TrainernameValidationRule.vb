5c5c08ebff3d44455b42ff04ce175416016a8e05
Andreas Studtrucker privat <studtrucker@posteo.de>
2024-12-17 08:03:13 +0100

    Validierung

---
+++ ValidationRules/TrainernameValidationRule.vb
@@ -0,0 +1,12 @@
+﻿Imports Groupies.Controller.AppController
+
+Public Class TrainernameValidationRule
+    Inherits ValidationRule
+
+    Public Overrides Function Validate(value As Object, cultureInfo As Globalization.CultureInfo) As ValidationResult
+        If CurrentClub.AlleTrainer.Select(Function(Tr) Tr.Spitzname).Contains(value) Then
+            Return New ValidationResult(False, $"Der Spitzname {value} darf nur einmal vorkommen")
+        End If
+        Return ValidationResult.ValidResult
+    End Function
+End Class

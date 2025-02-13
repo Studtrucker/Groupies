ec9f78a78fc858626e2679a3b48908bbb1aae7d1
Andreas Studtrucker <andreas.studtrucker@konecranes.com>
2025-02-13 09:51:53 +0100

    EInteilung

--- Windows/EinteilungDialog.xaml.vb
+++ Windows/EinteilungDialog.xaml.vb
@@ -1,5 +1,6 @@
 ﻿Imports Groupies.Interfaces
 Imports Groupies.Entities
+Imports Groupies.Services
 
 Public Class EinteilungDialog
     Implements Interfaces.IWindowMitModus
@@ -7,6 +8,17 @@ Public Class EinteilungDialog
     Property Einteilung As Einteilung
     Public Property Modus As Interfaces.IModus Implements Interfaces.IWindowMitModus.Modus
     Public Property Dialog As Boolean Implements Interfaces.IWindowMitModus.Dialog
+
+    Public Sub New()
+
+        ' Dieser Aufruf ist für den Designer erforderlich.
+        InitializeComponent()
+
+        ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
+        Einteilung = New Einteilung
+        DataContext = _Einteilung
+
+    End Sub
 
     Public Sub ModusEinstellen() Implements IWindowMitModus.ModusEinstellen
         Me.Titel.Text &= Modus.Titel
@@ -18,7 +30,13 @@ Public Class EinteilungDialog
         Return Fehlertext.Remove(Fehlertext.Count - 2, Len(vbNewLine))
     End Function
 
+    Private Sub NeueEinteilung()
+        _Einteilung.Benennung = "Tag2"
+        _Einteilung.Gruppenliste = TemplateService.StandardGruppenErstellen(5)
+    End Sub
+
     Private Sub HandlerOkButton(sender As Object, e As RoutedEventArgs)
+        NeueEinteilung()
         BindingGroup.CommitEdit()
         If Validation.GetHasError(Me) Then
             MessageBox.Show(GetErrors, "Ungültige Eingabe", MessageBoxButton.OK, MessageBoxImage.Error)
